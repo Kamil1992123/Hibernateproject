2187655db13c62cb29e68a0b06337a615c13cd5d
KAMIL <kamil123_1992@o2.pl>
2018-10-13 15:47:17 +0200

    first commit

---
+++ src/main/java/com/example/HIBERNATEproject/HibernatEprojectApplication.java
@@ -0,0 +1,12 @@
+package com.example.HIBERNATEproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernatEprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernatEprojectApplication.class, args);
+	}
+}
